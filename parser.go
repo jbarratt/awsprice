6f796638333949b7d04f7bd509451fee66de9829
Joshua Barratt <jbarratt@serialized.net>
2017-02-26 15:17:42 -0800

    Fixed single match search

--- parser.go
+++ parser.go
@@ -3,10 +3,12 @@ package awsprice
 // ParseInput takes a pricer and the input string and returns
 // a string representation of the price
 func ParseInput(pricer Pricer, input string) (string, error) {
-	price, err := pricer.Get(input, PriceAttr{})
+
 	// default region
 	region, _ := NewRegion("us-west-2")
 	attr := PriceAttr{Region: region}
+
+	price, err := pricer.Get(input, attr)
 	if err != nil {
 		prices := pricer.Search(input, attr)
 		if len(prices) == 0 {
